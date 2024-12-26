7ec9790e7e6d5db547fbdb79215097578b8eda1d
Jon King <159585551+illusconsulting@users.noreply.github.com>
2024-12-26 14:11:58 -0800

    20241226
    tshooting gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
     // https://vitejs.dev/config/
     export default defineConfig({
-      plugins: [vue()],
-      base: "/illus-attack-shuffle/"
+      base: "/illus-attack-shuffle/",
+      plugins: [vue()]
     })
 
